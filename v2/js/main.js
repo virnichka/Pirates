2ca848a25e1c7f609583a07e1e2a05be4ec1ea2f
virnichka <maxence.virchien@gmail.com>
2025-09-28 17:45:02 +0300

    Create main.js

---
+++ v2/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * 🚀 Point d’entrée du quiz
+ */
+
+window.onload = async () => {
+  try {
+    const accroches = await fetch("data/accroches.json").then(r => r.json());
+    const titre = randomItem(accroches.titres);
+    const sousTitre = randomItem(accroches.sousTitres);
+    document.getElementById("titre").innerText = titre;
+    document.getElementById("sousTitre").innerText = sousTitre;
+
+    const questions = await fetchQuestions();
+    if (questions.length > 0) startQuiz(questions);
+    else document.getElementById("quizQuestion").innerText = "Erreur de chargement du quiz.";
+  } catch (err) {
+    console.error("Erreur lors du démarrage :", err);
+  }
+};
+
+/**
+ * Renvoie un élément aléatoire d’un tableau
+ */
+function randomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
