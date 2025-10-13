6a312616fb50bf32463099ec8e14a4378bbb762b
virnichka <maxence.virchien@gmail.com>
2025-10-13 20:27:28 +0300

    Update quiz.js

--- v2/js/quiz.js
+++ v2/js/quiz.js
@@ -142,6 +142,14 @@ function showFinalScore() {
   const pourcentage = Math.round((score / total) * 100);
   const message = getCommentaire(pourcentage);
 
+  // 🧼 Cache le titre et le sous-titre pendant l'affichage du score final
+  const title = document.querySelector("header h1");
+  const subtitle = document.querySelector("header h2");
+  
+  if (title) title.style.display = "none";
+  if (subtitle) subtitle.style.display = "none";
+
+
   // 🧼 Nettoyer le texte explicatif à la fin du quiz
   const miniComment = document.getElementById("miniCommentaire");
   if (miniComment) {
