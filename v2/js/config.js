2aaddebf13d3d9b7839550c6ee49d52a94fa9220
virnichka <maxence.virchien@gmail.com>
2025-10-14 21:09:18 +0300

    Update config.js

--- v2/js/config.js
+++ v2/js/config.js
@@ -4,6 +4,6 @@
 const CONFIG = {
   GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx93i92e5L0HfuJIlfoU6ZSFHYT9cIJz5WfjOikscP2PJDGdoOHV8PuworGE4D0LGsb/exec",
   QUIZ_LIMIT: 5,
-  FULL_DARK_PASS: "FUCK" // 🗝️ mot de passe pour le mode Full Dark
+  FULL_DARK_PASS: "🖕" // 🗝️ mot de passe pour le mode Full Dark
 
 };
