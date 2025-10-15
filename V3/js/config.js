b2f54a67f5c60b163f0d553a81fd6fc9f163b672
virnichka <maxence.virchien@gmail.com>
2025-10-15 18:35:19 +0300

    Create config.js

---
+++ V3/js/config.js
@@ -0,0 +1,9 @@
+/**
+ * 🌍 Configuration globale du quiz
+ */
+const CONFIG = {
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx93i92e5L0HfuJIlfoU6ZSFHYT9cIJz5WfjOikscP2PJDGdoOHV8PuworGE4D0LGsb/exec",
+  QUIZ_LIMIT: 5,
+  FULL_DARK_PASS: "🖕" // 🗝️ mot de passe pour le mode Full Dark
+
+};
