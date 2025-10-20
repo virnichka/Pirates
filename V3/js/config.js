0412fa4d7e866079a0fe91898f5cade0505b1c7e
virnichka <maxence.virchien@gmail.com>
2025-10-20 23:13:57 +0300

    Update config.js

--- V3/js/config.js
+++ V3/js/config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx93i92e5L0HfuJIlfoU6ZSFHYT9cIJz5WfjOikscP2PJDGdoOHV8PuworGE4D0LGsb/exec",
   QUIZ_LIMIT: 5,
-  FULL_DARK_PASS: "🖕" // 🗝️ mot de passe pour le mode Full Dark
+  FULL_DARK_PASS: "🖕", // 🗝️ mot de passe pour le mode Full Dark
 
 
   window.CONFIG = CONFIG;
